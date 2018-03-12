70c9c86856948a96823fa51e7799ae4d4cf338b9
Munchurwoo <mdnbo1@naver.com>
2018-03-12 21:54:49 +0900

    commit

--- Java/Java_SourceCode/Lesson_1/src/Code_1.java
+++ Java/Java_SourceCode/Lesson_1/src/Code_1.java
@@ -3,7 +3,8 @@ public class Code_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("첫 출력");
+		int h =1 ;
+		String result = h == 3 ? "맞다" : "틀리다" ;
+		System.out.println(result);
 	}
-
 }
